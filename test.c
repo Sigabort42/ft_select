96d8093c3c0796fa36fbecc9b7de9705536bdf7f
Sigabort42 <elbenkrizi@gmail.com>
2018-04-28 23:23:51 +0200

    ft_select

---
+++ test.c
@@ -0,0 +1,92 @@
+#include "libft/libft.h"
+#include <curses.h>
+#include <term.h>
+#include <termios.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+void	ft_background_txt(char *bg, char *txt)
+{
+	tputs(tparm(bg, COLOR_WHITE), 1, ft_putchar);
+	tputs(tparm(txt, COLOR_BLACK), 1, ft_putchar);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (0);
+	char	*type_term = getenv("TERM");
+	char	*souligne;
+	char	*bg;
+	char	*txt;
+	char	*reset;
+	char	*cursor;
+	char	*clean;
+	char	buf[3];
+	struct termios lol;
+	int	i;
+	int	x;
+	int	ret;
+
+	i = 1;
+	x = 1;
+	if (tgetent(0, type_term) == ERR)
+		perror("tgetent");
+	bg = tgetstr("AB", NULL);
+	txt = tgetstr("AF", NULL);
+	souligne = tgetstr("us", NULL);
+	reset = tgetstr("me", NULL);
+	cursor = tgetstr("cm", NULL);
+	clean = tgetstr("cl", NULL);
+	tputs(clean, 1, ft_putchar);
+	tputs(tgoto(cursor, 0, 0), 1, ft_putchar);
+	while (argv[i])
+	{
+		if (i == x)
+		{
+			ft_background_txt(bg, txt);
+				tputs(souligne, 1, ft_putchar);
+		}
+		else
+			tputs(reset, 1, ft_putchar);
+		printf("%s\n", argv[i++]);
+	}
+	tcgetattr(0, &lol);
+	lol.c_lflag = ~(ICANON|ECHO);
+	lol.c_cc[VMIN] = 1;
+	lol.c_cc[VTIME] = 0;
+	tcsetattr(0, 0, &lol);
+	while ((ret = read(0, buf, 3)) > 0)
+	{
+		i = 1;
+		tputs(clean, 1, ft_putchar);
+		tputs(tgoto(cursor, 0, 0), 1, ft_putchar);
+		buf[ret + 1] = 0;
+		if (buf[2] == 66)
+			x++;
+		else if (buf[2] == 65)
+			x--;
+		while (argv[i])
+		{
+			if (i == x)
+			{
+				ft_background_txt(bg, txt);
+				tputs(souligne, 1, ft_putchar);
+			}
+			else
+				tputs(reset, 1, ft_putchar);
+			printf("%s\n", argv[i++]);
+		}
+		printf("\nbuf = %d %d %d\n", buf[0], buf[1], buf[2]);
+		bzero(buf, 3);
+	}
+	printf("ouloulouuuu\n");
+	printf("ouloulouuuu\n");
+//	tputs(cursor, 1, ft_putchar);
+//	tputs(tgoto(cursor, 0, 0), 1, ft_putchar);
+	printf("ouloulouuuu\n");
+	return (0);
+}
