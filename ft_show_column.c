e1c4cd04b6ad203e9fae8c1718b27b8d798b2ae9
Sigabort42 <elbenkrizi@gmail.com>
2018-05-13 22:36:43 +0200

    ft_select

--- ft_show_column.c
+++ ft_show_column.c
@@ -12,18 +12,44 @@
 
 #include "./includes/ft_select.h"
 
+static void     ft_color_file(char *av)
+{
+	struct stat     s;
+
+        if (stat(av, &s) == -1)
+		ft_putendl(av);
+        else if (S_ISLNK(s.st_mode))
+		ft_printf("{magenta}%s{eoc}\n", av);
+        else if (S_ISDIR(s.st_mode))
+		ft_printf("{cyan}%s{eoc}\n", av);
+        else if (S_ISCHR(s.st_mode))
+		ft_printf("{green}%s{eoc}\n", av);
+        else if (S_ISBLK(s.st_mode))
+		ft_printf("{blue}%s{eoc}\n", av);
+        else if (S_ISFIFO(s.st_mode))
+		ft_printf("{blue}%s{eoc}\n", av);
+        else if (S_ISSOCK(s.st_mode))
+		ft_printf("{yellow}%s{eoc}\n", av);
+	else if (s.st_mode & S_IXUSR)
+		ft_printf("{red}%s{eoc}\n", av);
+	else
+		ft_putendl(av);
+}
+
 void		ft_show_column2(t_env *env, char *name_node)
 {
 	if (env->nb_arg >= env->div && env->col < env->nb_arg / env->div)
 	{
 		tputs(tgoto(env->csr, env->row, env->col), 1, ft_putchar);
-		ft_putendl(name_node);
+		ft_color_file(name_node);
+//		ft_putendl(name_node);
 		env->col++;
 	}
 	else
 	{
 		tputs(tgoto(env->csr, env->row, env->col), 1, ft_putchar);
-		ft_putendl(name_node);
+		ft_color_file(name_node);
+//		ft_putendl(name_node);
 		env->row += env->size_word_max + 3;
 		env->col = 0;
 	}
